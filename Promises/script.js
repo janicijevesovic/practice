75bcc30560aeb1ed212d047a12236ab5577df18f
janicijevesovic <janicije95@hotmail.com>
2021-06-28 14:40:46 +0200

    Promises

--- Promises/script.js
+++ Promises/script.js
@@ -14,3 +14,89 @@ function getItemsReturnPromise(resource) {
         request.send();
     });
 }
+
+let output = () => {
+    getItemsReturnPromise("./fruits.json")
+    .then(data => {
+        data.forEach(item => {
+            console.log(item.name);
+        });
+        return getItemsReturnPromise("./vegetables.json");
+    })
+    .then(data => {
+        data.forEach(item => {
+            console.log(item.name);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
+// output();
+
+let outputDOM = () => {
+    getItemsReturnPromise("./fruits.json")
+    .then(data => {
+        let h3 = document.createElement('h3');
+        h3.textContent = "Fruits";
+        document.body.appendChild(h3);
+        let listFruits = document.createElement('ul');
+        data.forEach(item => {
+            let li = document.createElement('li');
+            li.textContent = item.name;
+            listFruits.appendChild(li);
+        });
+        document.body.appendChild(listFruits);
+        return getItemsReturnPromise("./vegetables.json");
+    })
+    .then(data => {
+        let h3 = document.createElement('h3');
+        h3.textContent = "Vegetables";
+        document.body.appendChild(h3);
+        let listVegetables = document.createElement('ul');
+        data.forEach(item => {
+            let li = document.createElement('li');
+            li.textContent = item.name;
+            listVegetables.appendChild(li);
+        });
+        document.body.appendChild(listVegetables);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
+// outputDOM();
+
+async function outputAsync() {
+    let data1 = await getItemsReturnPromise("./fruits.json");
+    let h3 = document.createElement('h3');
+    h3.textContent = "Fruits";
+    document.body.appendChild(h3);
+    let listFruits = document.createElement('ul');
+    data1.forEach(item => {
+        let li = document.createElement('li');
+        li.textContent = item.name;
+        listFruits.appendChild(li);
+    });
+    document.body.appendChild(listFruits);
+
+    let data2 = await getItemsReturnPromise("./vegetables.json");
+    h3 = document.createElement('h3');
+    h3.textContent = "Vegetables";
+    document.body.appendChild(h3);
+    let listVegetables = document.createElement('ul');
+    data2.forEach(item => {
+        let li = document.createElement('li');
+        li.textContent = item.name;
+        listVegetables.appendChild(li);
+    });
+    document.body.appendChild(listVegetables);
+}
+
+outputAsync()
+.then(() => {
+    console.log("Success");
+})
+.catch(err => {
+    console.log(err);
+});
