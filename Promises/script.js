23b467bd3992d86d82b05b6c31344eee070b5808
janicijevesovic <janicije95@hotmail.com>
2021-06-29 10:19:52 +0200

    Async

--- Promises/script.js
+++ Promises/script.js
@@ -73,28 +73,43 @@ async function outputAsync() {
     h3.textContent = "Fruits";
     document.body.appendChild(h3);
     let listFruits = document.createElement('ul');
+    let massFruits = 0;
     data1.forEach(item => {
+        massFruits += item.number;
         let li = document.createElement('li');
         li.textContent = item.name;
         listFruits.appendChild(li);
     });
+    let pFruits = document.createElement('p');
+    pFruits.textContent = `Fruit mass: ${massFruits}`;
     document.body.appendChild(listFruits);
+    document.body.appendChild(pFruits);
 
     let data2 = await getItemsReturnPromise("./vegetables.json");
     h3 = document.createElement('h3');
     h3.textContent = "Vegetables";
     document.body.appendChild(h3);
     let listVegetables = document.createElement('ul');
+    let massVegetables = 0;
     data2.forEach(item => {
+        massVegetables += item.number;
         let li = document.createElement('li');
         li.textContent = item.name;
         listVegetables.appendChild(li);
     });
+    let pVegetables = document.createElement('p');
+    pVegetables.textContent = `Vegetable mass: ${massVegetables}`;
     document.body.appendChild(listVegetables);
+    document.body.appendChild(pVegetables);
+
+    return massFruits + massVegetables;
 }
 
 outputAsync()
-.then(() => {
+.then(mass => {
+    let pMass = document.createElement('p');
+    pMass.textContent = `Sum mass: ${mass}`;
+    document.body.appendChild(pMass); 
     console.log("Success");
 })
 .catch(err => {
