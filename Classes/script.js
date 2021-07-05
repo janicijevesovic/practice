89b943ecc39b9bdeafe6d115a85beba1214c334e
janicijevesovic <janicije95@hotmail.com>
2021-07-05 18:41:01 +0200

    Classes - Functions

--- Classes/script.js
+++ Classes/script.js
@@ -1,8 +1,8 @@
 import { Student } from "./student.js";
 
-let student1 = new Student("1904995720019", "Pera", "Peric", [10, 9, 6]);
-let student2 = new Student("0202965725034", "Mika", "Mikic", [10, 9, 8]);
-let student3 = new Student("1206962720019", "Laza", "Lazic", [7, 7, 7, 6]);
+let student1 = new Student("1904995720019", "Pera", "Peric", [10, 7, 9]);
+let student2 = new Student("0202995725034", "Mika", "Mikic", [10, 9, 8]);
+let student3 = new Student("1206962720019", "Laza", "Lazic", [7, 7, 7, 9]);
 
 let arrStudent = [student1, student2, student3];
 
@@ -55,4 +55,55 @@ let najmanjaOcenaNajboljeg = arrStudent => {
         }
     });
     console.log(min);
+}
+
+let najmanjaOcena = arrStudent => {
+    let min = arrStudent[0].nizOcena[0];
+    arrStudent.forEach(student => {
+        student.nizOcena.forEach(ocena => {
+            if (ocena < min) {
+                min = ocena;
+            }
+        });
+    });
+    return min;
+}
+
+let najcescaOcena = arrStudent => {
+    let br6 = 0;
+    let br7 = 0;
+    let br8 = 0;
+    let br9 = 0;
+    let br10 = 0;
+    arrStudent.forEach(student => {
+        student.nizOcena.forEach(ocena => {
+            switch (ocena) {
+                case 6:
+                    br6 += 1;
+                    break;
+                case 7:
+                    br7 += 1;
+                    break;
+                case 8:
+                    br8 += 1;
+                    break;
+                case 9:
+                    br9 += 1;
+                    break;
+                case 10:
+                    br10 += 1;
+            }
+        });
+    });
+    let nizUkupno = [br6, br7, br8, br9, br10];
+    let max = Math.max(...nizUkupno);
+    return nizUkupno.indexOf(max) + 6;
+}
+
+let rodjeniGodine = (arrStudent, godina) => {
+    arrStudent.forEach(student => {
+        if (godina == "1" + student.jmbg.slice(4, 7)) {
+            console.log(student.ime, student.prezime);
+        }
+    });
 }
